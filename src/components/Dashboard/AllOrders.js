9e52bc83eab0cbdfe1c241769f46ca82ed49d4e0
merazul2755 <merazulehsan880@gmail.com>
2022-12-02 21:56:26 +0600

    40 commit

--- src/components/Dashboard/AllOrders.js
+++ src/components/Dashboard/AllOrders.js
@@ -209,7 +209,7 @@ export const AllOrders = () => {
                     htmlFor="my-modal"
                     className="flex items-center gap-1 text-white text-xs btn btn-xs px-4 dark:text-white btn-info"
                   >
-                    <svg
+                    {/* <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
@@ -220,7 +220,7 @@ export const AllOrders = () => {
                         d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z"
                         clipRule="evenodd"
                       />
-                    </svg>
+                    </svg> */}
                     {order.deliveryTime} min
                   </label>
                 ) : (
